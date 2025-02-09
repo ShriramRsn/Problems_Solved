cd5d6626928a8686631be9dbe9e3c172932f6ce8
Shriram R.S <shriramrs24@gmail.com>
2025-02-09 21:10:46 +0530

    first commit

---
+++ ArraysConcepts.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class ArraysConcepts {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        boolean[] stringsArr = new boolean[5];
+        System.out.println(Arrays.toString(stringsArr));
+    }
+}
