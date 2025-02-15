bf39bc0cbd0e43109db6eb8a154570ac658fdc2f
Shriram R.S <shriramrs24@gmail.com>
2025-02-15 23:40:13 +0530

    deleted files

--- Java/ArraysConcepts.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class ArraysConcepts {
-    public static void main(String[] args) {
-        int[] arr = new int[5];
-        boolean[] stringsArr = new boolean[5];
-        System.out.println(Arrays.toString(stringsArr));
-    }
-}
