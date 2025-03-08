39e4f5630d8bbab5a56acaea97ffec98608869de
Shriram R.S <shriramrs24@gmail.com>
2025-03-09 00:19:08 +0530

    Sum of digits problems

---
+++ Java/SumOfDigit.java
@@ -0,0 +1,36 @@
+public class SumOfDigit {
+    public static void main (String[] args){
+        System.out.println(sumOfDigit(12345));
+        int a = sumOfDigitOfThree(123);
+        int b = sumOfDigitOfThree(345);
+        int c = sumOfDigitOfThree(567);
+        System.out.print(a+b+c);
+    }
+    
+    static int sumOfDigit(int n){
+        int sum = 0;
+        while(n != 0){
+            int rem = n % 10;
+            sum =  sum + rem;
+            n /= 10;
+        }
+        n = sum;
+        sum = n % 10 + n / 10;
+        return sum;
+    }
+
+    static int sumOfDigitOfThree(int n){
+        int rem = 0, min = 0,max = 0;
+        while(n != 0){
+            rem = n % 10;
+            if(rem > max){
+                max = rem;
+            } min = rem;
+            if(rem < min){
+                min = rem;
+            }
+            n /= 10;
+        }
+        return min+max;
+    }
+}
