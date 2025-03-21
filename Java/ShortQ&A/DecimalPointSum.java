addef28d532e40907253c3e2e60c6f1738eb2ddc
Shriram R.S <shriramrs24@gmail.com>
2025-03-21 22:58:18 +0530

    sum of decimal places

---
+++ Java/ShortQ&A/DecimalPointSum.java
@@ -0,0 +1,34 @@
+public class DecimalPointSum {
+    public static void main(String[] args) {
+        double num = 12.257;
+        String str = Double.toString(num);
+
+        int dotIndex = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '.') {
+                dotIndex = i;
+                break;
+            }
+        }
+
+        String decimalPart = ""; 
+        int decimalPartInt = 0;
+
+        for (int i = dotIndex + 1; i < str.length(); i++) {
+            decimalPart += str.charAt(i);
+            int digit = str.charAt(i) - '0';
+            decimalPartInt = decimalPartInt * 10 + digit;
+        }
+
+        int sum = 0;
+        while(decimalPartInt != 0){
+            int rem = decimalPartInt % 10;
+            sum =  sum + rem;
+            decimalPartInt /= 10;
+        }
+
+        System.out.println(sum);
+        
+    }
+}
+
