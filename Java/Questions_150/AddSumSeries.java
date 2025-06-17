c09864e3ab928784f04540aeb40c5a9400683d9c
Shriram R S <96030653+ShriramRsn@users.noreply.github.com>
2025-06-17 14:56:50 +0530

    Addition & Subtraction of Series

---
+++ Java/Questions_150/AddSumSeries.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class AddSumSeries{
+    public static void main(String[] args){
+        int n = 6;
+        int opt = 2;
+        System.out.println(addSub(n,opt));
+    }
+
+    static int addSub(int input1,int input2){
+        int sum = input1;
+        boolean isPlus;
+        if(input2 == 2){
+            isPlus = True;
+        }
+        else{
+            isPlus = false;
+        }
+
+        for(int i = input1 - 1; i >= 1; i--){
+            if(isPlus){
+                sum += i;
+                isPlus = false;
+            }
+            else{
+                sum -= i;
+                isPlus = true;
+            }
+        }
+
+        return sum;
+    }
+}
