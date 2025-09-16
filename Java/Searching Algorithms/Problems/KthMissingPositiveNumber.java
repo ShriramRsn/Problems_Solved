1e214fd729462bf9aee0b8d91f1292f792dc2f53
ShriramRsn <shriramrs24@gmail.com>
2025-09-16 19:08:38 +0530

    1539. Kth Missing Positive Number

---
+++ Java/Searching Algorithms/Problems/KthMissingPositiveNumber.java
@@ -0,0 +1,19 @@
+public class KthMissingPositiveNumber {
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,7,11};
+        int k = 5;
+        System.out.println(findKthPositive(arr,k));
+    }
+    public static int findKthPositive(int[] arr, int k) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] - mid - 1 < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left + k;
+    }
+}
